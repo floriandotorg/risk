b6e5a46dfce1cd1bdd5ee53b84dd4e27d005869c
Florian Kaiser <mail@floriankaiser.org>
2024-02-17 19:32:44 +0100

    Fix formatting in Attack move debug output

--- src/game_state/display.rs
+++ src/game_state/display.rs
@@ -18,7 +18,7 @@ impl fmt::Debug for Move {
             Move::Pass => write!(f, "Pass"),
             Move::Reinforce{territory, armies} => write!(f, "Reinforce({} with {})", territory, armies),
             Move::Fortify{ from, to, armies} => write!(f, "Fortify({} from {} to {})", armies, from, to),
-            Move::Attack{ from, to, attacking, defending} => write!(f, "Attack({} with {} from {} defending with {})",  from, attacking, to, defending),
+            Move::Attack{ from, to, attacking, defending} => write!(f, "Attack(from {} with {} to {} defending with {})",  from, attacking, to, defending),
         }
     }
 }
